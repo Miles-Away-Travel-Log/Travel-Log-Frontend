8c980f3673f0b98b8093e9513e85844e5afb15fe
Elli Gnamm <elli.gnamm@gmail.com>
2022-04-05 14:11:00 +0200

    landingpage mobile

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,54 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
+import { route } from "next/dist/server/router";
 
 export default function Home() {
-    return <div className={styles.container}>Hallo Welt!</div>;
+    const router = useRouter();
+    function switchToRegister(e) {
+        e.preventDefault();
+        router.replace("/register");
+    }
+
+    return (
+        <div className={styles.landingpageContainer}>
+            <img src="./images/images-landingpage/dan-freeman-wAn4RfmXtxU-unsplash.jpg" />
+            <div className={styles.landingpageContainerLogo}>
+                <div className={styles.landingpageLogo}>
+                    <img src="./images/images-landingpage/logo.svg" />
+                </div>
+            </div>
+            <div className={styles.landingpageContainerAbout}>
+                <div className={styles.landingpageAboutText}>
+                    <p>
+                        ARE YOU GOING ON A TRIP ?
+                        <p>
+                            <br></br>
+                            <b>miles away</b> is exactly the right thing for you
+                            Record your most beautiful experiences in a diary
+                            and share them with your friends or with the whole
+                            wide world. With <b>miles away</b> you can always
+                            keep an eye on your budget as well as your
+                            experiences.
+                        </p>
+                    </p>
+                </div>
+                {/*placeholder*/}
+                <div className={styles.landingpageContainerGlobe}>
+                    <div className={styles.landingpageGlobe}>
+                        <img src="./images/images-landingpage/logo.svg" />
+                    </div>
+                </div>
+                <div className={styles.landingpageContainerButton}>
+                    <button
+                        onClick={switchToRegister}
+                        class="bg-[#90A5A9] hover:bg-[#C4C4C4] text-white font-bold py-2 px-4 rounded-full"
+                    >
+                        REGISTER NOW
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
