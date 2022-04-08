919641fb6eab83c81a3223aceef29ea593d39bc0
a-langner <andreas_langner@yahoo.de>
2022-04-08 15:58:19 +0200

    first test, still in part broken

--- Context/DataStorage.js
+++ Context/DataStorage.js
@@ -1,9 +1,9 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useReducer } from "react";
 
 const DataStorage = createContext();
 
 function AppState(props) {
-    const initialValues = {
+    const initialRegisterValues = {
         firstName: "",
         lastName: "",
         userName: "",
@@ -14,17 +14,49 @@ function AppState(props) {
         country: "",
     };
 
-    const [formValues, setFormValues] = useState(initialValues);
-    const [formErrors, setFormErrors] = useState({});
+    const markerTest = [
+        { name: "Andreas", visible: false, coordinates: {latitude: 51.3233680719318, longitude: 12.372418865370589} },
+        { name: "Albert", visible: false, coordinates: {latitude: 51.33907051907917, longitude: 12.401449243834573} },
+        { name: "Connect", visible: false },
+    ];
+
+    const mapMarkerTestReducer = (originalArray, action) => {
+        switch (action.type) {
+            case "toggle":
+                const toggleArray = [...originalArray];
+                return toggleArray.map(item => {
+                    if (item.name === action.name) {
+                        return {...item, visible: !action.visible}
+                    }
+                return item;})
+                break;
+          
+            default:
+                return originalArray
+                break;
+        }
+    };
+
+    const [registerFormValues, setRegisterFormValues] = useState(
+        initialRegisterValues
+    );
+    const [registerFormErrors, setRegisterFormErrors] = useState({});
+    const [mapLayoutTest, setMapLayoutTest] = useState(
+        "mapbox://styles/mapbox/streets-v9"
+    );
+    const [mapMarkerTest, dispatchMapMarkerTest] = useReducer(mapMarkerTestReducer ,markerTest);
 
     return (
         <DataStorage.Provider
             value={{
-                initialValues,
-                formValues,
-                setFormValues,
-                formErrors,
-                setFormErrors,
+                registerFormValues,
+                setRegisterFormValues,
+                registerFormErrors,
+                setRegisterFormErrors,
+                mapLayoutTest,
+                setMapLayoutTest,
+                mapMarkerTest,
+                dispatchMapMarkerTest,
             }}
         >
             {props.children}
---
+++ components/map-dummy-selection.jsx
@@ -0,0 +1,139 @@
+import React, { useEffect } from "react";
+import { useAppData } from "../Context/DataStorage.js";
+
+export default function MapDummySelection() {
+    const { setMapLayoutTest, dispatchMapMarkerTest, mapMarkerTest } =
+        useAppData();
+
+    const handleChangeRadio = (event) => {
+        setMapLayoutTest(event.target.value);
+    };
+
+    const handleChangeCheckbox = (event) => {
+        dispatchMapMarkerTest({
+            type: "toggle",
+            name: event.target.value,
+            visible: event.target.checked,
+        });
+        console.log(mapMarkerTest);
+        console.log("value: ", event.target.value);
+        console.log("id: ", event.target.id);
+        console.log("checked: ", event.target.checked);
+    };
+
+    let check0 = mapMarkerTest[0].visible;
+    let check1 = mapMarkerTest[1].visible;
+
+    useEffect(() => {
+        console.log(mapMarkerTest);
+    }, [mapMarkerTest]);
+
+    return (
+        <div className="h-auto w-full">
+            <section className="bg-gray-400 p-1 flex justify-around">
+                <div>
+                    <label htmlFor="option1" className="pr-2">
+                        Standard-Layout
+                    </label>
+                    <input
+                        id="option1"
+                        type="radio"
+                        name="test"
+                        value="mapbox://styles/mapbox/streets-v9"
+                        checked={true}
+                        onChange={handleChangeRadio}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="option2" className="pr-2">
+                        Night-Blue
+                    </label>
+                    <input
+                        id="option2"
+                        type="radio"
+                        name="test"
+                        value="mapbox://styles/miles-away/cl1m7c4fs000214qjqkr8jmue"
+                        onChange={handleChangeRadio}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="option3" className="pr-2">
+                        Satellite
+                    </label>
+                    <input
+                        id="option3"
+                        type="radio"
+                        name="test"
+                        value="mapbox://styles/miles-away/cl1nee67n005914pnjtmoo86s"
+                        onChange={handleChangeRadio}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="option4" className="pr-2">
+                        Bubblegum
+                    </label>
+                    <input
+                        id="option4"
+                        type="radio"
+                        name="test"
+                        value="mapbox://styles/miles-away/cl1nld29v00hb15qslsepe5tm"
+                        onChange={handleChangeRadio}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="option5" className="pr-2">
+                        Outdoors
+                    </label>
+                    <input
+                        id="option5"
+                        type="radio"
+                        name="test"
+                        value="mapbox://styles/miles-away/cl1nlk66s000314qj8rr9efd6"
+                        onChange={handleChangeRadio}
+                    />
+                </div>
+            </section>
+            <section className="bg-gray-400 p-1 flex justify-around">
+                <div>
+                    <label htmlFor="option6" className="pr-2">
+                        Andreas
+                    </label>
+                    <input
+                        id="option6"
+                        type="checkbox"
+                        name="test-2"
+                        value="Andreas"
+                        onChange={(event) =>
+                            dispatchMapMarkerTest({
+                                type: "toggle",
+                                name: event.target.value,
+                                visible: check0,
+                            })
+                        }
+                        checked={check0}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="option7" className="pr-2">
+                        Albert
+                    </label>
+                    <input
+                        id="option7"
+                        type="checkbox"
+                        name="test-3"
+                        value="Albert"
+                        onChange={(event) =>
+                            dispatchMapMarkerTest({
+                                type: "toggle",
+                                name: event.target.value,
+                                visible: check1,
+                            })
+                        }
+                        checked={check1}
+                    />
+                </div>
+                {/* {console.log(mapMarkerTest)} */}
+            </section>
+        </div>
+    );
+}
