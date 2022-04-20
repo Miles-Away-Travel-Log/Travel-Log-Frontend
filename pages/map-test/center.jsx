f6d001c62803c271f91583e95d816662c51a73b3
a-langner <andreas_langner@yahoo.de>
2022-04-20 16:08:21 +0200

    small index.js fix + minor map-test update

--- pages/map-test/center.jsx
+++ pages/map-test/center.jsx
@@ -1,21 +1,22 @@
 import MapDummy from "../../components/map-dummy.jsx";
-import getCenter from "geolib/es/getCenter";
+import getCenterOfBounds from "geolib/es/getCenter";
 import Map, { Marker, Popup } from "react-map-gl";
 import { useState, useEffect } from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
 
+const testCoordinatesArray = [
+    { longitude: 13.377722, latitude: 52.516272, name: "Berlin" },
+    { longitude: -0.119722, latitude: 51.503333, name: "London" },
+    { longitude: 2.352222, latitude: 48.856614, name: "Paris" },
+    { longitude: -73.94, latitude: 40.7127,name: "New York" },
+];
+
 export default function MapTestCenter() {
     const [selectedLocation, setSelectedLocation] = useState({});
 
-    const testCoordinatesArray = [
-        { longitude: 13.377722, latitude: 52.516272, name: "Berlin" },
-        { longitude: -0.119722, latitude: 51.503333, name: "London" },
-        { longitude: 2.352222, latitude: 48.856614, name: "Paris" },
-    ];
+    const center = getCenterOfBounds(testCoordinatesArray);
 
-    const center = getCenter(testCoordinatesArray);
-
-    useEffect(() => {console.log(selectedLocation)}, [selectedLocation]);
+    // useEffect(() => {console.log(selectedLocation)}, [selectedLocation]);
 
     return (
         <div className="h-screen w-screen">
@@ -40,27 +41,28 @@ export default function MapTestCenter() {
                             anchor="bottom"
                         >
                             <p
-                                onClick={() => setSelectedLocation(result)
-                                }
+                                onClick={(e) => {
+                                    e.stopPropagation();            // super wichtig, sonst funktioniert die Popups nicht
+                                    setSelectedLocation(result)
+                                }}
                                 className="cursor-pointer text-2xl animate-bounce"
                             >
                                 {/* 📍 */}
                                 🔻
                             </p>
                         </Marker>
-                        {selectedLocation.longitude === result.longitude ? 
-                            <Popup
-                                // onClose={() => setSelectedLocation({})}
-                                // closeOnClick={true}
-                                latitude={result.latitude}
-                                longitude={result.longitude}
-                            >
-                                {result.name}
-                                test
-
-                            </Popup>
-                         : (
-                            false
+                        {selectedLocation.longitude === result.longitude && (
+                            <div>
+                                <Popup
+                                    // onClose={() => setSelectedLocation({})}
+                                    // closeOnClick={true}
+                                    latitude={result.latitude}
+                                    longitude={result.longitude}
+                                    // onClose={() => console.log("close")}
+                                >
+                                    {result.name}
+                                </Popup>
+                            </div>
                         )}
                     </div>
                 ))}
