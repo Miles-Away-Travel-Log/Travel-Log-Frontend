261b97752d84b288a469a95b7fb3add96a6af60f
Elli Gnamm <elli.gnamm@gmail.com>
2022-04-28 13:51:25 +0200

    menu done

---
+++ pages/weatherdummy.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './Header';
+
+const Page = () => {
+    return (
+        <Fragment>
+            <Header />
+            <div></div>
+        </Fragment>
+    );
+    
+};
+
+export default Page;
