21237270d89b39c36a93fe49f403c98c8ef4fc59
Patricia Grammetbauer <info@PatriciaGrammetbauer.de>
2022-05-02 10:30:43 +0200

    icons gestylt

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,9 @@
 import { useRouter } from "next/router";
 import React, { useEffect, useState, useRef } from "react";
+import { icons, clickables } from "./menu.jsx";
 
 function Navbar() {
     const router = useRouter();
-
-    function navigateToLogin() {
-        router.replace("/login");
-    }
 
     const [buttonWidth, setButtonWidth] = useState(0);
     // const [menuWidth, setMenuWidth] = useState(0);
@@ -22,10 +19,10 @@ function Navbar() {
         setButtonWidth(buttonRef.current.offsetWidth);
         // setMenuWidth(menuRef.current.offsetWidth);
     }, []);
-
+   
+    
     return (
         <div className="App">
-            {/* <menu className="menu" ref={menuRef}> */}
             <menu className="menu">
                 <button
                     ref={buttonRef}
@@ -36,35 +33,35 @@ function Navbar() {
                         backgroundColor: "#90A5A9",
                         marginRight: marginButtons,
                     }}
-                    onClick={(() => setButtonIndex(0), navigateToLogin)}
+                    onClick={(() => setButtonIndex(0), ()=> router.replace("/user/landingpageUser"))}
                 >
-                    <svg className="icon" viewBox="0 0 24 24">
-                        <path d="M3.8,6.6h16.4" />
-                        <path d="M20.2,12.1H3.8" />
-                        <path d="M3.8,17.5h16.4" />
-                    </svg>
+                    <div
+                        className="icon navIcon text-white flex justify-center "
+                        viewBox="0 0 24 24"
+                    >
+                        {clickables[4].icon}
+                        {/* {icons.FaUserAlt} */}
+                    </div>
                 </button>
+
                 <button
+                    ref={buttonRef}
                     className={
                         buttonIndex === 1 ? "menu__item active" : "menu__item"
                     }
+                    style={{
+                        backgroundColor: "#90A5A9",
+                        marginRight: marginButtons,
+                    }}
                 >
-                    <svg
-                        className="icon"
+                    <div
+                        className="icon navIcon text-white flex justify-center flex"
                         viewBox="0 0 24 24"
-                        style={{
-                            backgroundColor: "#90A5A9",
-                            marginRight: marginButtons,
-                        }}
-                        onClick={() => setButtonIndex(1)}
+                        onClick={(() => setButtonIndex(1), ()=> router.replace("/user/budget"))}
                     >
-                        <path
-                            d="M6.7,4.8h10.7c0.3,0,0.6,0.2,0.7,0.5l2.8,7.3c0,0.1,0,0.2,0,0.3v5.6c0,0.4-0.4,0.8-0.8,0.8H3.8
-        C3.4,19.3,3,19,3,18.5v-5.6c0-0.1,0-0.2,0.1-0.3L6,5.3C6.1,5,6.4,4.8,6.7,4.8z"
-                        />
-                        <path d="M3.4,12.9H8l1.6,2.8h4.9l1.5-2.8h4.6" />
-                    </svg>
-                </button>
+                        {clickables[0].icon}
+                    </div>
+                        </button>
 
                 <button
                     className={
@@ -74,13 +71,15 @@ function Navbar() {
                         backgroundColor: "#90A5A9",
                         marginRight: marginButtons,
                     }}
-                    onClick={() => setButtonIndex(2)}
+                    onClick={(() => setButtonIndex(2), ()=> router.replace("/user/friends"))}
                 >
-                    <svg className="icon" viewBox="0 0 24 24">
-                        <path d="M3.4,11.9l8.8,4.4l8.4-4.4" />
-                        <path d="M3.4,16.2l8.8,4.5l8.4-4.5" />
-                        <path d="M3.7,7.8l8.6-4.5l8,4.5l-8,4.3L3.7,7.8z" />
-                    </svg>
+                          <div
+                        className="icon navIcon text-white flex justify-center flex"
+                        viewBox="0 0 24 24"
+                        onClick={(() => setButtonIndex(1))}
+                    >
+                        {clickables[1].icon}
+                    </div>
                 </button>
 
                 <button
@@ -93,14 +92,13 @@ function Navbar() {
                     }}
                     onClick={() => setButtonIndex(3)}
                 >
-                    <svg className="icon" viewBox="0 0 24 24">
-                        <path
-                            d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1
-          C3.9,4.4,4.4,3.9,5.1,3.9z"
-                        />
-                        <path d="M4.2,9.3h15.6" />
-                        <path d="M9.1,9.5v10.3" />
-                    </svg>
+                    <div
+                        className="icon navIcon text-white flex justify-center flex"
+                        viewBox="0 0 24 24"
+                        onClick={(() => setButtonIndex(3))}
+                    >
+                        {clickables[9].icon}
+                    </div>
                 </button>
 
                 <button
@@ -113,14 +111,13 @@ function Navbar() {
                     }}
                     onClick={() => setButtonIndex(4)}
                 >
-                    <svg className="icon" viewBox="0 0 24 24">
-                        <path
-                            d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1
-          C3.9,4.4,4.4,3.9,5.1,3.9z"
-                        />
-                        <path d="M5.5,20l9.9-9.9l4.7,4.7" />
-                        <path d="M10.4,8.8c0,0.9-0.7,1.6-1.6,1.6c-0.9,0-1.6-0.7-1.6-1.6C7.3,8,8,7.3,8.9,7.3C9.7,7.3,10.4,8,10.4,8.8z" />
-                    </svg>
+                    <div
+                        className="icon navIcon text-white flex justify-center flex"
+                        viewBox="0 0 24 24"
+                        onClick={(() => setButtonIndex(1))}
+                    >
+                        {clickables[11].icon}
+                    </div>
                 </button>
 
                 <button
@@ -130,14 +127,13 @@ function Navbar() {
                     style={{ backgroundColor: "#90A5A9" }}
                     onClick={() => setButtonIndex(5)}
                 >
-                    <svg className="icon" viewBox="0 0 24 24">
-                        <path
-                            d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1
-          C3.9,4.4,4.4,3.9,5.1,3.9z"
-                        />
-                        <path d="M5.5,20l9.9-9.9l4.7,4.7" />
-                        <path d="M10.4,8.8c0,0.9-0.7,1.6-1.6,1.6c-0.9,0-1.6-0.7-1.6-1.6C7.3,8,8,7.3,8.9,7.3C9.7,7.3,10.4,8,10.4,8.8z" />
-                    </svg>
+                    <div
+                        className="icon navIcon text-white flex justify-center flex"
+                        viewBox="0 0 24 24"
+                        onClick={(() => setButtonIndex(5),()=> router.replace("/user/diary"))}
+                    >
+                        {clickables[12].icon}
+                    </div>
                 </button>
 
                 <div
