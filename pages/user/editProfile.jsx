b0ea9dd38348d1041381df399c0b7fb25cda2598
Patricia Grammetbauer <info@PatriciaGrammetbauer.de>
2022-05-04 10:14:37 +0200

    delete consolelog

--- pages/user/editProfile.jsx
+++ pages/user/editProfile.jsx
@@ -19,7 +19,7 @@ export default function EditProfile() {
         defaultMapStyle,
         setDefaultMapStyle,
     } = useAppData();
-    console.log(user);
+  
     const userInitialValues = {
         firstName: "",
         lastName: "",
