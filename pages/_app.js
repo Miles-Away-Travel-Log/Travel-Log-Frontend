434a8efa9b9bc28f70b371a106a85590337ce999
Elli Gnamm <elli.gnamm@gmail.com>
2022-05-11 11:43:26 +0200

    wetter eingebaut

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import Styles from "../styles/navbar.css";
 import { AppState } from "../Context/DataStorage.js";
+import "../styles/weather.scss";
 
 function MyApp({ Component, pageProps }) {
     return (
